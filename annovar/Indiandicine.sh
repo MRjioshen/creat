78062e07beb13fe9c4a70167bf85ee5ddb680ef6
unknown <zjhzs163@gmail.com>
2024-09-05 18:37:32 +0800

    第一次提交

---
+++ annovar/Indiandicine.sh
@@ -0,0 +1,1 @@
+/stor9000/apps/users/NWSUAF/2016060159/software/annovar/annovar/convert2annovar.pl -format vcf4 /stor9000/apps/users/NWSUAF/2015010869/zhaojiahao/analyze/1.SNP/2.listgz/Indiandicine.vcf.gz -withfreq -allsample -o Indiandicine.avinput
